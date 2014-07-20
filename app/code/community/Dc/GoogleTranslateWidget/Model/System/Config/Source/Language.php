384ba1ec04b7887ac951c0cf4303cfee9deff5b5
Damián Culotta <damianculotta@gmail.com>
2014-07-20 14:34:46 -0300

    Fixed languages sort on module configuration. Removed version number on each file.

--- app/code/community/Dc/GoogleTranslateWidget/Model/System/Config/Source/Language.php
+++ app/code/community/Dc/GoogleTranslateWidget/Model/System/Config/Source/Language.php
@@ -12,276 +12,97 @@
  * @package    Dc_GoogleTranslateWidget
  * @copyright  Copyright (c) 2014 Damián Culotta. (http://www.damianculotta.com.ar/)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @version    1.0.0
  */
 
 class Dc_GoogleTranslateWidget_Model_System_Config_Source_Language
 {
 
+    protected $_languages = array();
+
     /**
      * Translations between the languages listed in the following table are supported
      * https://developers.google.com/translate/v2/using_rest?hl=es
      */
+    public function getLanguages()
+    {
+        if (!count($this->_languages) > 0) {
+            $this->_languages['af'] = Mage::helper('googletranslatewidget')->__('Afrikaans');
+            $this->_languages['sq'] = Mage::helper('googletranslatewidget')->__('Albanian');
+            $this->_languages['ar'] = Mage::helper('googletranslatewidget')->__('Arabic');
+            $this->_languages['az'] = Mage::helper('googletranslatewidget')->__('Azerbaijani');
+            $this->_languages['eu'] = Mage::helper('googletranslatewidget')->__('Basque');
+            $this->_languages['bn'] = Mage::helper('googletranslatewidget')->__('Bengali');
+            $this->_languages['be'] = Mage::helper('googletranslatewidget')->__('Belarusian');
+            $this->_languages['bg'] = Mage::helper('googletranslatewidget')->__('Bulgarian');
+            $this->_languages['ca'] = Mage::helper('googletranslatewidget')->__('Catalan');
+            $this->_languages['zh-CN'] = Mage::helper('googletranslatewidget')->__('Chinese (Simplified)');
+            $this->_languages['zh-TW'] = Mage::helper('googletranslatewidget')->__('Chinese (Traditional)');
+            $this->_languages['hr'] = Mage::helper('googletranslatewidget')->__('Croatian');
+            $this->_languages['cs'] = Mage::helper('googletranslatewidget')->__('Czech');
+            $this->_languages['da'] = Mage::helper('googletranslatewidget')->__('Danish');
+            $this->_languages['nl'] = Mage::helper('googletranslatewidget')->__('Dutch');
+            $this->_languages['en'] = Mage::helper('googletranslatewidget')->__('English');
+            $this->_languages['eo'] = Mage::helper('googletranslatewidget')->__('Esperanto');
+            $this->_languages['et'] = Mage::helper('googletranslatewidget')->__('Estonian');
+            $this->_languages['tl'] = Mage::helper('googletranslatewidget')->__('Filipino');
+            $this->_languages['fi'] = Mage::helper('googletranslatewidget')->__('Finnish');
+            $this->_languages['fr'] = Mage::helper('googletranslatewidget')->__('French');
+            $this->_languages['gl'] = Mage::helper('googletranslatewidget')->__('Galician');
+            $this->_languages['ka'] = Mage::helper('googletranslatewidget')->__('Georgian');
+            $this->_languages['de'] = Mage::helper('googletranslatewidget')->__('German');
+            $this->_languages['el'] = Mage::helper('googletranslatewidget')->__('Greek');
+            $this->_languages['gu'] = Mage::helper('googletranslatewidget')->__('Gujarati');
+            $this->_languages['ht'] = Mage::helper('googletranslatewidget')->__('Haitian Creole');
+            $this->_languages['iw'] = Mage::helper('googletranslatewidget')->__('Hebrew');
+            $this->_languages['hi'] = Mage::helper('googletranslatewidget')->__('Hindi');
+            $this->_languages['hu'] = Mage::helper('googletranslatewidget')->__('Hungarian');
+            $this->_languages['is'] = Mage::helper('googletranslatewidget')->__('Icelandic');
+            $this->_languages['id'] = Mage::helper('googletranslatewidget')->__('Indonesian');
+            $this->_languages['ga'] = Mage::helper('googletranslatewidget')->__('Irish');
+            $this->_languages['it'] = Mage::helper('googletranslatewidget')->__('Italian');
+            $this->_languages['ja'] = Mage::helper('googletranslatewidget')->__('Japanese');
+            $this->_languages['kn'] = Mage::helper('googletranslatewidget')->__('Kannada');
+            $this->_languages['ko'] = Mage::helper('googletranslatewidget')->__('Korean');
+            $this->_languages['la'] = Mage::helper('googletranslatewidget')->__('Latin');
+            $this->_languages['lv'] = Mage::helper('googletranslatewidget')->__('Latvian');
+            $this->_languages['lt'] = Mage::helper('googletranslatewidget')->__('Lithuanian');
+            $this->_languages['mk'] = Mage::helper('googletranslatewidget')->__('Macedonian');
+            $this->_languages['ms'] = Mage::helper('googletranslatewidget')->__('Malay');
+            $this->_languages['mt'] = Mage::helper('googletranslatewidget')->__('Maltese');
+            $this->_languages['no'] = Mage::helper('googletranslatewidget')->__('Norweigan');
+            $this->_languages['fa'] = Mage::helper('googletranslatewidget')->__('Persian');
+            $this->_languages['pl'] = Mage::helper('googletranslatewidget')->__('Polish');
+            $this->_languages['pt'] = Mage::helper('googletranslatewidget')->__('Portuguese');
+            $this->_languages['ro'] = Mage::helper('googletranslatewidget')->__('Romanian');
+            $this->_languages['ru'] = Mage::helper('googletranslatewidget')->__('Russian');
+            $this->_languages['sr'] = Mage::helper('googletranslatewidget')->__('Serbian');
+            $this->_languages['sk'] = Mage::helper('googletranslatewidget')->__('Slovak');
+            $this->_languages['sl'] = Mage::helper('googletranslatewidget')->__('Slovenian');
+            $this->_languages['es'] = Mage::helper('googletranslatewidget')->__('Spanish');
+            $this->_languages['sw'] = Mage::helper('googletranslatewidget')->__('Swahili');
+            $this->_languages['sv'] = Mage::helper('googletranslatewidget')->__('Swedish');
+            $this->_languages['ta'] = Mage::helper('googletranslatewidget')->__('Tamil');
+            $this->_languages['te'] = Mage::helper('googletranslatewidget')->__('Telugu');
+            $this->_languages['th'] = Mage::helper('googletranslatewidget')->__('Thai');
+            $this->_languages['tr'] = Mage::helper('googletranslatewidget')->__('Turkish');
+            $this->_languages['uk'] = Mage::helper('googletranslatewidget')->__('Ukrainian');
+            $this->_languages['ur'] = Mage::helper('googletranslatewidget')->__('Urdu');
+            $this->_languages['vi'] = Mage::helper('googletranslatewidget')->__('Vietnamese');
+            $this->_languages['cy'] = Mage::helper('googletranslatewidget')->__('Welsh');
+            $this->_languages['yi'] = Mage::helper('googletranslatewidget')->__('Yiddish');
+        }
+        asort($this->_languages, SORT_STRING | SORT_FLAG_CASE);
+        return $this->_languages;
+    }
+
     public function toOptionArray()
     {
-        $options = array(
-                    array(
-                        'value' => 'af',
-                        'label' => Mage::helper('googletranslatewidget')->__('Afrikaans')
-                    ),
-                    array(
-                        'value' => 'sq',
-                        'label' => Mage::helper('googletranslatewidget')->__('Albanian')
-                    ),
-                    array(
-                        'value' => 'ar',
-                        'label' => Mage::helper('googletranslatewidget')->__('Arabic')
-                    ),
-                    array(
-                        'value' => 'az',
-                        'label' => Mage::helper('googletranslatewidget')->__('Azerbaijani')
-                    ),
-                    array(
-                        'value' => 'eu',
-                        'label' => Mage::helper('googletranslatewidget')->__('Basque')
-                    ),
-                    array(
-                        'value' => 'bn',
-                        'label' => Mage::helper('googletranslatewidget')->__('Bengali')
-                    ),
-                    array(
-                        'value' => 'be',
-                        'label' => Mage::helper('googletranslatewidget')->__('Belarusian')
-                    ),
-                    array(
-                        'value' => 'bg',
-                        'label' => Mage::helper('googletranslatewidget')->__('Bulgarian')
-                    ),
-                    array(
-                        'value' => 'ca',
-                        'label' => Mage::helper('googletranslatewidget')->__('Catalan')
-                    ),
-                    array(
-                        'value' => 'zh-CN',
-                        'label' => Mage::helper('googletranslatewidget')->__('Chinese (Simplified)')
-                    ),
-                    array(
-                        'value' => 'zh-TW',
-                        'label' => Mage::helper('googletranslatewidget')->__('Chinese (Traditional)')
-                    ),
-                    array(
-                        'value' => 'hr',
-                        'label' => Mage::helper('googletranslatewidget')->__('Croatian')
-                    ),
-                    array(
-                        'value' => 'cs',
-                        'label' => Mage::helper('googletranslatewidget')->__('Czech')
-                    ),
-                    array(
-                        'value' => 'da',
-                        'label' => Mage::helper('googletranslatewidget')->__('Danish')
-                    ),
-                    array(
-                        'value' => 'nl',
-                        'label' => Mage::helper('googletranslatewidget')->__('Dutch')
-                    ),
-                    array(
-                        'value' => 'en',
-                        'label' => Mage::helper('googletranslatewidget')->__('English')
-                    ),
-                    array(
-                        'value' => 'eo',
-                        'label' => Mage::helper('googletranslatewidget')->__('Esperanto')
-                    ),
-                    array(
-                        'value' => 'et',
-                        'label' => Mage::helper('googletranslatewidget')->__('Estonian')
-                    ),
-                    array(
-                        'value' => 'tl',
-                        'label' => Mage::helper('googletranslatewidget')->__('Filipino')
-                    ),
-                    array(
-                        'value' => 'fi',
-                        'label' => Mage::helper('googletranslatewidget')->__('Finnish')
-                    ),
-                    array(
-                        'value' => 'fr',
-                        'label' => Mage::helper('googletranslatewidget')->__('French')
-                    ),
-                    array(
-                        'value' => 'gl',
-                        'label' => Mage::helper('googletranslatewidget')->__('Galician')
-                    ),
-                    array(
-                        'value' => 'ka',
-                        'label' => Mage::helper('googletranslatewidget')->__('Georgian')
-                    ),
-                    array(
-                        'value' => 'de',
-                        'label' => Mage::helper('googletranslatewidget')->__('German')
-                    ),
-                    array(
-                        'value' => 'el',
-                        'label' => Mage::helper('googletranslatewidget')->__('Greek')
-                    ),
-                    array(
-                        'value' => 'gu',
-                        'label' => Mage::helper('googletranslatewidget')->__('Gujarati')
-                    ),
-                    array(
-                        'value' => 'ht',
-                        'label' => Mage::helper('googletranslatewidget')->__('Haitian Creole')
-                    ),
-                    array(
-                        'value' => 'iw',
-                        'label' => Mage::helper('googletranslatewidget')->__('Hebrew')
-                    ),
-                    array(
-                        'value' => 'hi',
-                        'label' => Mage::helper('googletranslatewidget')->__('Hindi')
-                    ),
-                    array(
-                        'value' => 'hu',
-                        'label' => Mage::helper('googletranslatewidget')->__('Hungarian')
-                    ),
-                    array(
-                        'value' => 'is',
-                        'label' => Mage::helper('googletranslatewidget')->__('Icelandic')
-                    ),
-                    array(
-                        'value' => 'id',
-                        'label' => Mage::helper('googletranslatewidget')->__('Indonesian')
-                    ),
-                    array(
-                        'value' => 'ga',
-                        'label' => Mage::helper('googletranslatewidget')->__('Irish')
-                    ),
-                    array(
-                        'value' => 'it',
-                        'label' => Mage::helper('googletranslatewidget')->__('Italian')
-                    ),
-                    array(
-                        'value' => 'ja',
-                        'label' => Mage::helper('googletranslatewidget')->__('Japanese')
-                    ),
-                    array(
-                        'value' => 'kn',
-                        'label' => Mage::helper('googletranslatewidget')->__('Kannada')
-                    ),
-                    array(
-                        'value' => 'ko',
-                        'label' => Mage::helper('googletranslatewidget')->__('Korean')
-                    ),
-                    array(
-                        'value' => 'la',
-                        'label' => Mage::helper('googletranslatewidget')->__('Latin')
-                    ),
-                    array(
-                        'value' => 'lv',
-                        'label' => Mage::helper('googletranslatewidget')->__('Latvian')
-                    ),
-                    array(
-                        'value' => 'lt',
-                        'label' => Mage::helper('googletranslatewidget')->__('Lithuanian')
-                    ),
-                    array(
-                        'value' => 'mk',
-                        'label' => Mage::helper('googletranslatewidget')->__('Macedonian')
-                    ),
-                    array(
-                        'value' => 'ms',
-                        'label' => Mage::helper('googletranslatewidget')->__('Malay')
-                    ),
-                    array(
-                        'value' => 'mt',
-                        'label' => Mage::helper('googletranslatewidget')->__('Maltese')
-                    ),
-                    array(
-                        'value' => 'no',
-                        'label' => Mage::helper('googletranslatewidget')->__('Norweigan')
-                    ),
-                    array(
-                        'value' => 'fa',
-                        'label' => Mage::helper('googletranslatewidget')->__('Persian')
-                    ),
-                    array(
-                        'value' => 'pl',
-                        'label' => Mage::helper('googletranslatewidget')->__('Polish')
-                    ),
-                    array(
-                        'value' => 'pt',
-                        'label' => Mage::helper('googletranslatewidget')->__('Portuguese')
-                    ),
-                    array(
-                        'value' => 'ro',
-                        'label' => Mage::helper('googletranslatewidget')->__('Romanian')
-                    ),
-                    array(
-                        'value' => 'ru',
-                        'label' => Mage::helper('googletranslatewidget')->__('Russian')
-                    ),
-                    array(
-                        'value' => 'sr',
-                        'label' => Mage::helper('googletranslatewidget')->__('Serbian')
-                    ),
-                    array(
-                        'value' => 'sk',
-                        'label' => Mage::helper('googletranslatewidget')->__('Slovak')
-                    ),
-                    array(
-                        'value' => 'sl',
-                        'label' => Mage::helper('googletranslatewidget')->__('Slovenian')
-                    ),
-                    array(
-                        'value' => 'es',
-                        'label' => Mage::helper('googletranslatewidget')->__('Spanish')
-                    ),
-                    array(
-                        'value' => 'sw',
-                        'label' => Mage::helper('googletranslatewidget')->__('Swahili')
-                    ),
-                    array(
-                        'value' => 'sv',
-                        'label' => Mage::helper('googletranslatewidget')->__('Swedish')
-                    ),
-                    array(
-                        'value' => 'ta',
-                        'label' => Mage::helper('googletranslatewidget')->__('Tamil')
-                    ),
-                    array(
-                        'value' => 'te',
-                        'label' => Mage::helper('googletranslatewidget')->__('Telugu')
-                    ),
-                    array(
-                        'value' => 'th',
-                        'label' => Mage::helper('googletranslatewidget')->__('Thai')
-                    ),
-                    array(
-                        'value' => 'tr',
-                        'label' => Mage::helper('googletranslatewidget')->__('Turkish')
-                    ),
-                    array(
-                        'value' => 'uk',
-                        'label' => Mage::helper('googletranslatewidget')->__('Ukrainian')
-                    ),
-                    array(
-                        'value' => 'ur',
-                        'label' => Mage::helper('googletranslatewidget')->__('Urdu')
-                    ),
-                    array(
-                        'value' => 'vi',
-                        'label' => Mage::helper('googletranslatewidget')->__('Vietnamese')
-                    ),
-                    array(
-                        'value' => 'cy',
-                        'label' => Mage::helper('googletranslatewidget')->__('Welsh')
-                    ),
-                    array(
-                        'value' => 'yi',
-                        'label' => Mage::helper('googletranslatewidget')->__('Yiddish')
-                    )
-        );
+        foreach ($this->getLanguages() as $code => $label) {
+            $options[] = array(
+                        'value' => $code,
+                        'label' => $label
+                    ); 
+        }
         return $options;
     }
 
